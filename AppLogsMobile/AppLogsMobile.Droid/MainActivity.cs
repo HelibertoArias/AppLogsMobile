9004d6aa11eb95483160a98155edda63d5230946
helibertoarias <helibertoarias@gmail.com>
2016-12-18 12:38:33 -0500

    - Cambio de logo en la aplicacion Droid. - Se adiciona logos generados desde https://romannurik.github.io/AndroidAssetStudio/icons-launcher.html

--- AppLogsMobile/AppLogsMobile.Droid/MainActivity.cs
+++ AppLogsMobile/AppLogsMobile.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace AppLogsMobile.Droid
 {
-    [Activity(Label = "AppLogsMobile", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "App Logs", Icon = "@drawable/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
